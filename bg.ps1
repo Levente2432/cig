6a683d3a5ea0f1dc0a6e39ecbaf74215324ba9d8
Levente2432 <148475736+Levente2432@users.noreply.github.com>
2026-02-10 00:38:44 +0100

    a
    a

---
+++ bg.ps1
@@ -0,0 +1,6 @@
+$u = 'https://example.com/bg.jpg'      # prank image
+$p = "$env:TEMP\bg.jpg"
+Invoke-WebRequest $u -OutFile $p
+
+Add-Type 'using System;using System.Runtime.InteropServices;public class W{[DllImport("user32.dll")]public static extern int SystemParametersInfo(int a,int b,string c,int d);}'
+[W]::SystemParametersInfo(20,0,$p,3)
