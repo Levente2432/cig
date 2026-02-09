46938c850a4af5ff2fbf867971269a5cbc6c4fc3
Levente2432 <148475736+Levente2432@users.noreply.github.com>
2026-02-10 00:30:25 +0100

    a
    a

--- m.ps1
+++ m.ps1
@@ -1,9 +1,8 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-
 while ($true) {
+    $screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
     $x = Get-Random -Minimum 0 -Maximum $screen.Width
     $y = Get-Random -Minimum 0 -Maximum $screen.Height
     [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
