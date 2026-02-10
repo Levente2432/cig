787a29df325475d7f3933089115c83d373158984
Levente2432 <148475736+Levente2432@users.noreply.github.com>
2026-02-10 02:22:18 +0100

    a
    a

--- bg.ps1
+++ bg.ps1
@@ -1,5 +1,5 @@
 $u = 'https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg'      # prank image
-$p = "$env:TEMP\bg.jpg"
+$p = "$env:TEMP\bg1.jpg"
 Invoke-WebRequest $u -OutFile $p
 
 Add-Type 'using System;using System.Runtime.InteropServices;public class W{[DllImport("user32.dll")]public static extern int SystemParametersInfo(int a,int b,string c,int d);}'
