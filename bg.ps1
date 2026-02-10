5414deefffa9cbb18c65b3dc8391d21facf615a1
Levente2432 <148475736+Levente2432@users.noreply.github.com>
2026-02-10 02:23:12 +0100

    a
    a

--- bg.ps1
+++ bg.ps1
@@ -1,5 +1,5 @@
-$u = 'https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg'      # prank image
-$p = "$env:TEMP\bg1.jpg"
+$u = 'https://i1.sndcdn.com/artworks-Tylvt34Y9xym3HW8-FK97Ew-t500x500.png'      # prank image
+$p = "$env:TEMP\bg2.jpg"
 Invoke-WebRequest $u -OutFile $p
 
 Add-Type 'using System;using System.Runtime.InteropServices;public class W{[DllImport("user32.dll")]public static extern int SystemParametersInfo(int a,int b,string c,int d);}'
