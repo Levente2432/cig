ea2c9d4eb01920bf812b0b67b3953f0d6b62fc12
Levente2432 <148475736+Levente2432@users.noreply.github.com>
2026-02-10 02:27:56 +0100

    Update bg.ps1 to use unique filenames - fixes Windows wallpaper caching issue

--- bg.ps1
+++ bg.ps1
@@ -1,5 +1,6 @@
 $u = 'https://i1.sndcdn.com/artworks-Tylvt34Y9xym3HW8-FK97Ew-t500x500.png'      # prank image
-$p = "$env:TEMP\bga.jpg"
+$timestamp = Get-Date -Format "yyyyMMddHHmmss"
+$p = "$env:TEMP\bga_$timestamp.jpg"
 Invoke-WebRequest $u -OutFile $p
 
 Add-Type 'using System;using System.Runtime.InteropServices;public class W{[DllImport("user32.dll")]public static extern int SystemParametersInfo(int a,int b,string c,int d);}'
