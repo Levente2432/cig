a1bfb3beb73890944a1a645a98b987239c7eb1c8
Levente2432 <148475736+Levente2432@users.noreply.github.com>
2026-03-15 23:26:57 +0100

    a
    a

--- cigpopup.ps1
+++ cigpopup.ps1
@@ -1,7 +1,7 @@
 Add-Type -AssemblyName Microsoft.VisualBasic
 $r=[Microsoft.VisualBasic.Interaction]::MsgBox('Cigany vagy?',4+32,'Cigany')
 if($r-eq6){
-  [Microsoft.VisualBasic.Interaction]::MsgBox('Jo fiu',64,'Yes')
+  [Microsoft.VisualBasic.Interaction]::MsgBox('Tudom',64,'Yes')
 }else{
   [Microsoft.VisualBasic.Interaction]::MsgBox('Hat pedig az vagy',64,'No')
 }
